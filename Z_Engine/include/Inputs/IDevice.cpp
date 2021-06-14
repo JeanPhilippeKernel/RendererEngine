c77eefacb6fde65506f5540418729474b1dac3b4
Konan Jean Philippe Aubin Kouassi <kkouassi@microsoft.com>
2021-06-14 21:39:59 +0100

    removed old dependencies

--- Z_Engine/include/Inputs/IDevice.cpp
+++
@@ -1,9 +0,0 @@
-#include "IDevice.h"
-
-namespace Z_Engine::Inputs {
-
-		std::unordered_map<const char*, Ref<IDevice>> IDevice::m_devices;
-}
-
-
-
--- Z_Engine/include/Window/CoreWindow.cpp
+++
@@ -1,5 +0,0 @@
-#include "CoreWindow.h"
-
-namespace Z_Engine::Window {
-	const char* CoreWindow::ATTACHED_PROPERTY = "WINDOW_ATTACHED_PROPERTY";
-}
