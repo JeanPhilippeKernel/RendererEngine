e794285113ed9f2f2e52a9c0f2e02278632e7306
Jean Philippe <jeanphilippe02666592@outlook.fr>
2021-12-01 09:56:27 +0100

    added client logger support (#159)

--- ZEngine/include/ZEngine/Logging/Logger.h
+++ ZEngine/include/ZEngine/Logging/Logger.h
@@ -10,9 +10,11 @@ namespace ZEngine::Logging {
         Logger(const Logger&) = delete;
 
         static void                 Initialize();
-        static Ref<spdlog::logger>& GetCurrent();
+        static Ref<spdlog::logger>& GetEngineLogger();
+        static Ref<spdlog::logger>& GetEditorLogger();
 
     private:
         static Ref<spdlog::logger> m_logger;
+        static Ref<spdlog::logger> m_editor_logger;
     };
 } // namespace ZEngine::Logging
