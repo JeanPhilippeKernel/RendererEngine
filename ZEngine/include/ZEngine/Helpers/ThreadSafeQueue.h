f90294167324ade06b029a83ec29847e63247f9f
Jennifer Chukwu <jnyfaah@gmail.com>
2024-01-24 17:24:00 +0000

    threadpool utility (#305)
    * threadpool utility
    
    * code review
    
    * fix tests
    
    * fix tests
    
    * fix bugs

---
+++ ZEngine/include/ZEngine/Helpers/ThreadSafeQueue.h
@@ -0,0 +1,70 @@
+#include <condition_variable>
+#include <functional>
+#include <mutex>
+#include <queue>
+
+namespace ZEngine::Helpers
+{
+    template <typename T>
+    class ThreadSafeQueue
+    {
+    public:
+        void Enqueue(const T& task)
+        {
+            std::unique_lock<std::mutex> lock(m_mutex);
+            m_queue.push(task);
+            m_condition.notify_one();
+        }
+
+        bool Pop(T& task)
+        {
+            std::unique_lock<std::mutex> lock(m_mutex);
+
+            if (m_queue.empty())
+            {
+                return false;
+            }
+
+            task = std::move(m_queue.front());
+            m_queue.pop();
+            return true;
+        }
+
+        bool Empty() const
+        {
+            std::lock_guard<std::mutex> lock(m_mutex);
+            return m_queue.empty();
+        }
+
+        size_t Size() const
+        {
+            std::lock_guard<std::mutex> lock(m_mutex);
+            return m_queue.size();
+        }
+
+        void Wait()
+        {
+            std::unique_lock<std::mutex> lock(m_mutex);
+            if (m_queue.empty())
+            {
+                m_condition.wait(lock, [this] {
+                    return !m_queue.empty();
+                });
+            }
+        }
+
+        void Clear()
+        {
+            std::unique_lock<std::mutex>      lock(m_mutex);
+            std::queue<T> empty;
+            std::swap(m_queue, empty);
+            m_condition.notify_all();
+        }
+
+    private:
+        mutable std::mutex                m_mutex;
+        std::condition_variable           m_condition;
+        std::queue<T> m_queue;
+    };
+
+} // namespace ZEngine::Helpers
---
+++ ZEngine/tests/ThreadPool_test.cpp
@@ -0,0 +1,54 @@
+#include <gtest/gtest.h>
+#include "Helpers/ThreadPool.h"
+
+using namespace ZEngine::Helpers;
+ 
+class ThreadPoolTest : public ::testing::Test {
+protected:
+    void SetUp() override {
+    }
+ 
+    void TearDown() override {
+        ThreadPoolHelper::Shutdown();
+    }
+};
+ 
+TEST_F(ThreadPoolTest, Submit) {
+    std::atomic<bool> executed = false;
+    ThreadPoolHelper::Submit([&executed] {
+        executed = true;
+    });
+ 
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+ 
+    EXPECT_TRUE(executed);
+}
+ 
+TEST_F(ThreadPoolTest, TaskExecution) {
+    std::atomic<int> counter = 0;
+    for (int i = 0; i < 5; ++i) {
+        ThreadPoolHelper::Submit([&counter] {
+            counter++;
+        });
+    }
+ 
+    std::this_thread::sleep_for(std::chrono::milliseconds(500));
+ 
+    EXPECT_EQ(counter, 5);
+}
+ 
+TEST_F(ThreadPoolTest, MultipleTasksExecution) {
+    std::atomic<int> counter = 0;
+    const int numberOfTasks = 10;
+ 
+    for (int i = 0; i < numberOfTasks; ++i) {
+        ThreadPoolHelper::Submit([&counter] {
+            std::this_thread::sleep_for(std::chrono::milliseconds(100)); 
+            ++counter;
+        });
+    }
+ 
+    std::this_thread::sleep_for(std::chrono::seconds(2));
+ 
+    EXPECT_EQ(counter, numberOfTasks);
+}
