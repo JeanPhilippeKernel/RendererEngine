b1e5801615bc6752b8771a481d0f620ddebb69ae
jnyfah <jnyfaah@gmail.com>
2024-07-14 17:13:32 +0100

    fmt format

--- ZEngine/src/ShaderManager.cpp
+++ ZEngine/src/ShaderManager.cpp
@@ -4,47 +4,47 @@
 
 namespace ZEngine::Managers
 {
-    std::unordered_map<std::string, Ref<Rendering::Shaders::Shader>> ShaderManager::m_shaderMappings = {};
+    std::unordered_map<std::string, Ref<Rendering::Shaders::Shader>> ShaderManager::m_shader_mappings = {};
 
     Ref<Rendering::Shaders::Shader> ShaderManager::Get(ZEngine::Rendering::Specifications::ShaderSpecification spec)
     {
-        spec.VertexFilename = GetVertexFilename(spec.Name);
+        spec.VertexFilename   = GetVertexFilename(spec.Name);
         spec.FragmentFilename = GetFragmentFilename(spec.Name);
 
-        auto& shader = m_shaderMappings[spec.Name];
+        auto& shader = m_shader_mappings[spec.Name];
         if (!shader)
         {
-            shader = ZEngine::Rendering::Shaders::Shader::Create(spec);
-            m_shaderMappings[spec.Name] = shader;
+            shader                       = ZEngine::Rendering::Shaders::Shader::Create(spec);
+            m_shader_mappings[spec.Name] = shader;
         }
         return shader;
     }
 
-    const std::string ShaderManager::base_dir = "Shaders/Cache/";
+    const std::string_view ShaderManager::base_dir = "Shaders/Cache/";
 
-    const std::unordered_map<std::string, std::pair<std::string, std::string>> ShaderManager::m_shaderPath = {
+    const std::unordered_map<std::string, std::pair<std::string, std::string>> ShaderManager::m_shader_path = {
+        {"color", {fmt::format("{}final_color_vertex.spv", base_dir), fmt::format("{}final_color_fragment.spv", base_dir)}},
+        {"g_buffer", {fmt::format("{}g_buffer_vertex.spv", base_dir), fmt::format("{}g_buffer_fragment.spv", base_dir)}},
+        {"imgui", {fmt::format("{}imgui_vertex.spv", base_dir), fmt::format("{}imgui_fragment.spv", base_dir)}},
+        {"infinite_grid", {fmt::format("{}infinite_grid_vertex.spv", base_dir), fmt::format("{}infinite_grid_fragment.spv", base_dir)}},
+        {"skybox", {fmt::format("{}skybox_vertex.spv", base_dir), fmt::format("{}skybox_fragment.spv", base_dir)}},
+        {"depth_prepass_scene", {fmt::format("{}depth_prepass_scene_vertex.spv", base_dir), ""}},
+        {"deferred_lighting", {fmt::format("{}deferred_lighting_vertex.spv", base_dir), fmt::format("{}deferred_lighting_fragment.spv", base_dir)}}};
 
-        {"color", {base_dir + "final_color_vertex.spv", base_dir + "final_color_fragment.spv"}},
-        {"g_buffer", {base_dir + "g_buffer_vertex.spv", base_dir + "g_buffer_fragment.spv"}},
-        {"imgui", {base_dir + "imgui_vertex.spv", base_dir + "imgui_fragment.spv"}},
-        {"infinite_grid", {base_dir + "infinite_grid_vertex.spv", base_dir + "infinite_grid_fragment.spv"}},
-        {"skybox", {base_dir + "skybox_vertex.spv", base_dir + "skybox_fragment.spv"}},
-        {"depth_prepass_scene", {base_dir + "depth_prepass_scene_vertex.spv", ""}}};
-
-    const std::string ShaderManager::GetFragmentFilename(std::string_view key)
+    const std::string ShaderManager::GetFragmentFilename(const std::string_view key)
     {
-        auto it = m_shaderPath.find(key.data());
-        if (it == m_shaderPath.end())
+        auto it = m_shader_path.find(key.data());
+        if (it == m_shader_path.end())
         {
             return "";
         }
         return it->second.second;
     }
 
-    const std::string ShaderManager::GetVertexFilename(std::string_view key)
+    const std::string ShaderManager::GetVertexFilename(const std::string_view key)
     {
-        auto it = m_shaderPath.find(key.data());
-        if (it == m_shaderPath.end())
+        auto it = m_shader_path.find(key.data());
+        if (it == m_shader_path.end())
         {
             return "";
         }
@@ -62,13 +62,13 @@ namespace ZEngine::Managers
         Ref<Rendering::Shaders::Shader> shader;
         shader.reset(Rendering::Shaders::CreateShader(filename, true));
 
-        std::random_device rd;
-        std::ranlux48_base generator(rd());
+        std::random_device                                     rd;
+        std::ranlux48_base                                     generator(rd());
         uuids::basic_uuid_random_generator<std::ranlux48_base> gen(&generator);
 
-        uuids::uuid const guid = gen();
-        const auto key = uuids::to_string(guid) + m_suffix;
-        const auto result = IManager::Add(key, std::move(shader));
+        uuids::uuid const guid   = gen();
+        const auto        key    = uuids::to_string(guid) + m_suffix;
+        const auto        result = IManager::Add(key, std::move(shader));
 
         assert(result.has_value() == true);
         return result->get();
@@ -77,7 +77,7 @@ namespace ZEngine::Managers
     Ref<Rendering::Shaders::Shader> ShaderManager::Load(const char* filename)
     {
         std::filesystem::path p(filename);
-        const auto name = p.stem();
+        const auto            name = p.stem();
         return Add(reinterpret_cast<const char*>(name.u8string().c_str()), filename);
     }
 } // namespace ZEngine::Managers
