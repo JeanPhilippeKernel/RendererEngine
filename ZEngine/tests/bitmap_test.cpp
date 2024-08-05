0f11d07a9021797ca7f824612ef8d48727ff76be
jnyfah <jnyfaah@gmail.com>
2024-08-05 11:04:30 +0100

    bitmap test revert

--- ZEngine/tests/bitmap_test.cpp
+++ ZEngine/tests/bitmap_test.cpp
@@ -33,17 +33,16 @@ TEST(BitmapTest, GetOrSetPixel)
 
 TEST(BitmapTest, TestVerticalCross)
 {
-    int          width = 0, height = 0, channel = 0;
-    float        x          = 5.0f;
-    const float* image_data = &x;
+int          width = 0, height = 0, channel = 0;
+    const float* image_data = stbi_loadf("piazza_bologni_1k.hdr", &width, &height, &channel, 3);
 
     Bitmap in             = {width, height, channel, BitmapFormat::FLOAT, image_data};
     Bitmap vertical_cross = Bitmap::EquirectangularMapToVerticalCross(in);
     Bitmap cubemap        = Bitmap::VerticalCrossToCubemap(vertical_cross);
-    //stbi_image_free((void*) image_data);
+    stbi_image_free((void*) image_data);
 
-    //stbi_write_hdr("screenshot.hdr", vertical_cross.Width, vertical_cross.Height, vertical_cross.Channel, (const float*) vertical_cross.Buffer.data());
-    //stbi_write_hdr("screenshot2.hdr", cubemap.Width, cubemap.Height, cubemap.Channel, (const float*) cubemap.Buffer.data());
+    stbi_write_hdr("screenshot.hdr", vertical_cross.Width, vertical_cross.Height, vertical_cross.Channel, (const float*) vertical_cross.Buffer.data());
+    stbi_write_hdr("screenshot2.hdr", cubemap.Width, cubemap.Height, cubemap.Channel, (const float*) cubemap.Buffer.data());
 
     auto current_path = std::filesystem::current_path().string();
 
