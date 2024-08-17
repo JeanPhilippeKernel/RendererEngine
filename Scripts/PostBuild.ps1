443fb942e82602c4a6fd844d32cf113d43fe9145
Jean Philippe <jeanphilippe02666592@outlook.fr>
2024-08-17 14:44:27 +0100

    updated build proc and lauch for vscode

--- Scripts/PostBuild.ps1
+++ Scripts/PostBuild.ps1
@@ -28,8 +28,8 @@ param (
     [string[]] $SystemName = 'Windows',
 
     [Parameter(HelpMessage="Architecture type to build, default to x64")]
-    [ValidateSet('x64', 'arm64', 'osx-x64', 'osx-arm64')]
-    [string[]] $Architectures = 'x64',
+    [ValidateSet('win-x64', 'arm64', 'osx-x64', 'osx-arm64')]
+    [string[]] $Architectures = 'win-x64',
 
     [Parameter(HelpMessage="Configuration type to build, default to Debug")]
     [ValidateSet('Debug', 'Release')]
@@ -83,7 +83,7 @@ $ContentsToProcess = @(
         Contents = @(
             switch ($SystemName) {
                 "Windows" {
-                    @{ From = "$OuputBuildDirectory\Tetragrama\src\$Configurations"; To = "$OuputBuildDirectory\Panzerfaust\$Configurations\net6.0\Editor"}
+                    @{ From = "$OuputBuildDirectory\Tetragrama\src\$Configurations"; To = "$OuputBuildDirectory\Panzerfaust\$Configurations\net6.0\$Architectures\publish\Editor"}
                 }
                 "Darwin" {
                     switch ($Architectures) {
