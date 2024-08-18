eda26f6ee710d01b640e71dc73c9250ad46c22f3
Jean Philippe <jeanphilippe02666592@outlook.fr>
2024-08-18 20:43:34 +0100

    Updating build process and launch.json definition for VSCode (#373)
    * updated build proc and lauch for vscode
    
    * updated launcher json
    
    * cleaned up launcher
    
    * fixed publish output of launcher
    
    * fixed target output name
    
    * fixed post build for macos env

--- Scripts/PostBuild.ps1
+++ Scripts/PostBuild.ps1
@@ -28,8 +28,8 @@ param (
     [string[]] $SystemName = 'Windows',
 
     [Parameter(HelpMessage="Architecture type to build, default to x64")]
-    [ValidateSet('x64', 'arm64', 'osx-x64', 'osx-arm64')]
-    [string[]] $Architectures = 'x64',
+    [ValidateSet('win-x64', 'arm64', 'osx-x64', 'osx-arm64')]
+    [string[]] $Architectures = 'win-x64',
 
     [Parameter(HelpMessage="Configuration type to build, default to Debug")]
     [ValidateSet('Debug', 'Release')]
@@ -83,15 +83,15 @@ $ContentsToProcess = @(
         Contents = @(
             switch ($SystemName) {
                 "Windows" {
-                    @{ From = "$OuputBuildDirectory\Tetragrama\src\$Configurations"; To = "$OuputBuildDirectory\Panzerfaust\$Configurations\net6.0\Editor"}
+                    @{ From = "$OuputBuildDirectory\Tetragrama\src\$Configurations"; To = "$OuputBuildDirectory\Panzerfaust\$Configurations\net6.0\$Architectures\publish\Editor"}
                 }
                 "Darwin" {
                     switch ($Architectures) {
                         "osx-x64" {
-                            @{ From = "$OuputBuildDirectory\Tetragrama\src\$Configurations"; To = "$OuputBuildDirectory\Panzerfaust\$Configurations\net6.0\$Architectures\Editor"}
+                            @{ From = "$OuputBuildDirectory\Tetragrama\src\$Configurations"; To = "$OuputBuildDirectory\Panzerfaust\$Configurations\net6.0\$Architectures\publish\Editor"}
                         }
                         "osx-arm64" {
-                            @{ From = "$OuputBuildDirectory\Tetragrama\src\$Configurations"; To = "$OuputBuildDirectory\Panzerfaust\$Configurations\net6.0\$Architectures\Editor"}
+                            @{ From = "$OuputBuildDirectory\Tetragrama\src\$Configurations"; To = "$OuputBuildDirectory\Panzerfaust\$Configurations\net6.0\$Architectures\publish\Editor"}
                         }
                         Default {
                             throw 'This architecture is not supported'
