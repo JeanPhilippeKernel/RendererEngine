9935bcc30031a62f543f8674d6d31952a78ffb6d
Jean Philippe <jeanphilippe02666592@outlook.fr>
2024-08-19 17:20:39 +0100

    fixed engine path (#375)

--- Scripts/PostBuild.ps1
+++ Scripts/PostBuild.ps1
@@ -83,14 +83,17 @@ $ContentsToProcess = @(
         Contents = @(
             switch ($SystemName) {
                 "Windows" {
+                    @{ From = "$OuputBuildDirectory\Tetragrama\src\$Configurations"; To = "$OuputBuildDirectory\Panzerfaust\$Configurations\net6.0\Editor"}
                     @{ From = "$OuputBuildDirectory\Tetragrama\src\$Configurations"; To = "$OuputBuildDirectory\Panzerfaust\$Configurations\net6.0\$Architectures\publish\Editor"}
                 }
                 "Darwin" {
                     switch ($Architectures) {
                         "osx-x64" {
+                            @{ From = "$OuputBuildDirectory\Tetragrama\src\$Configurations"; To = "$OuputBuildDirectory\Panzerfaust\$Configurations\net6.0\$Architectures\Editor"}
                             @{ From = "$OuputBuildDirectory\Tetragrama\src\$Configurations"; To = "$OuputBuildDirectory\Panzerfaust\$Configurations\net6.0\$Architectures\publish\Editor"}
                         }
                         "osx-arm64" {
+                            @{ From = "$OuputBuildDirectory\Tetragrama\src\$Configurations"; To = "$OuputBuildDirectory\Panzerfaust\$Configurations\net6.0\$Architectures\Editor"}
                             @{ From = "$OuputBuildDirectory\Tetragrama\src\$Configurations"; To = "$OuputBuildDirectory\Panzerfaust\$Configurations\net6.0\$Architectures\publish\Editor"}
                         }
                         Default {
@@ -103,6 +106,41 @@ $ContentsToProcess = @(
                 }
                 Default {
                     throw 'This system is not supported'
+                }
+            }
+        )
+    },
+    @{
+        Name = "Examples"
+        IsDirectory = $true
+        Contents = @(
+            if ($Configurations -eq "Debug") {
+                switch ($SystemName) {
+                    "Windows" {
+                        @{ From = "$RepoRoot\Examples"; To = "$OuputBuildDirectory\Panzerfaust\$Configurations\net6.0\Examples"}
+                        @{ From = "$RepoRoot\Examples"; To = "$OuputBuildDirectory\Panzerfaust\$Configurations\net6.0\$Architectures\publish\Examples"}
+                    }
+                    "Darwin" {
+                        switch ($Architectures) {
+                            "osx-x64" {
+                                @{ From = "$RepoRoot\Examples"; To = "$OuputBuildDirectory\Panzerfaust\$Configurations\net6.0\$Architectures\publish\Examples"}
+                                @{ From = "$RepoRoot\Examples"; To = "$OuputBuildDirectory\Panzerfaust\$Configurations\net6.0\$Architectures\Examples"}
+                            }
+                            "osx-arm64" {
+                                @{ From = "$RepoRoot\Examples"; To = "$OuputBuildDirectory\Panzerfaust\$Configurations\net6.0\$Architectures\publish\Examples"}
+                                @{ From = "$RepoRoot\Examples"; To = "$OuputBuildDirectory\Panzerfaust\$Configurations\net6.0\$Architectures\Examples"}
+                            }
+                            Default {
+                                throw 'This architecture is not supported'
+                            }
+                        }
+                    }
+                    "Linux" {
+                        @{ From = "$RepoRoot\Examples"; To = "$OuputBuildDirectory\Panzerfaust\$Configurations\net6.0\Examples"}
+                    }
+                    Default {
+                        throw 'This system is not supported'
+                    }
                 }
             }
         )
