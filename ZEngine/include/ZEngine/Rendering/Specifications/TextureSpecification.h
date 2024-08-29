c692a0d01f5572f4958e23337867cf6cace4a696
jnyfah <jnyfaah@gmail.com>
2024-08-29 14:57:02 +0100

    added Image2DBufferSpecification constructor

--- ZEngine/include/ZEngine/Rendering/Specifications/TextureSpecification.h
+++ ZEngine/include/ZEngine/Rendering/Specifications/TextureSpecification.h
@@ -1,6 +1,6 @@
 #pragma once
-#include <ZEngineDef.h>
 #include <Rendering/Specifications/FormatSpecification.h>
+#include <ZEngineDef.h>
 
 namespace ZEngine::Rendering::Specifications
 {
@@ -30,6 +30,20 @@ namespace ZEngine::Rendering::Specifications
         VkImageAspectFlagBits image_aspect_flag;
         uint32_t              layer_count           = 1U;
         VkImageCreateFlags    image_create_flag_bit = 0;
+
+        Image2DBufferSpecification(
+            uint32_t              w,
+            uint32_t              h,
+            ImageType             view_type,
+            VkFormat              format,
+            VkImageUsageFlags     usage,
+            VkImageAspectFlagBits aspect,
+            uint32_t              layers       = 1U,
+            VkImageCreateFlags    create_flags = 0)
+            : width(w), height(h), image_view_type(view_type), image_format(format), image_usage(usage), image_aspect_flag(aspect), layer_count(layers),
+              image_create_flag_bit(create_flags)
+        {
+        }
     };
 
 } // namespace ZEngine::Rendering::Specifications
