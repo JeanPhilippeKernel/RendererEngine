940da9693a82c7ab54d06d43947917c43a4ac64a
Jean Philippe <jeanphilippe02666592@outlook.fr>
2024-10-17 14:03:28 +0100

    Canonical project restructure (#381)
    * reorganized project structure
    
    * deleted noisy code
    
    * restructured editor
    
    * fixed post build script

---
+++ ZEngine/ZEngine/Rendering/Entities/GraphicSceneEntity.h
@@ -0,0 +1,98 @@
+#pragma once
+#include <Logging/LoggerDefinition.h>
+#include <ZEngineDef.h>
+#include <entt/entt.hpp>
+#include <type_traits>
+
+namespace ZEngine::Rendering::Scenes
+{
+    struct SceneRawData;
+}
+
+namespace ZEngine::Rendering::Entities
+{
+    class GraphicSceneEntity
+    {
+    public:
+        static GraphicSceneEntity CreateWrapper(const std::shared_ptr<entt::registry>& registry_ptr, entt::entity handle);
+
+    public:
+        GraphicSceneEntity() = default;
+        GraphicSceneEntity(entt::entity handle);
+        GraphicSceneEntity(GraphicSceneEntity&& rhs) noexcept;
+        virtual ~GraphicSceneEntity() = default;
+
+        GraphicSceneEntity& operator=(GraphicSceneEntity&& rhs) noexcept;
+
+        template <typename TComponent>
+        bool HasComponent() const
+        {
+            if (s_weak_registry_ptr.expired())
+            {
+                return false;
+            }
+            auto registry = s_weak_registry_ptr.lock();
+            return registry->all_of<TComponent>(m_entity_handle);
+        }
+
+        template <typename TComponent, typename... Args>
+        TComponent& AddComponent(Args&&... args)
+        {
+            if (HasComponent<TComponent>())
+            {
+                ZENGINE_CORE_ERROR("This component has already been added to this entity")
+                return GetComponent<TComponent>();
+            }
+            auto registry = s_weak_registry_ptr.lock();
+            return registry->emplace<TComponent>(m_entity_handle, std::forward<Args>(args)...);
+        }
+
+        template <typename TComponent>
+        void RemoveComponent()
+        {
+            auto registry = s_weak_registry_ptr.lock();
+            registry->remove<TComponent>(m_entity_handle);
+        }
+
+        template <typename TComponent>
+        TComponent& GetComponent() const
+        {
+            auto registry = s_weak_registry_ptr.lock();
+            return registry->get<TComponent>(m_entity_handle);
+        }
+
+        bool operator==(const GraphicSceneEntity& rhs)
+        {
+            return (this->m_entity_handle == rhs.m_entity_handle) && (this->s_weak_registry_ptr.lock().get() == rhs.s_weak_registry_ptr.lock().get());
+        }
+
+        bool operator!=(const GraphicSceneEntity& rhs)
+        {
+            return !((*this) == rhs);
+        }
+
+        operator uint32_t() const
+        {
+            return static_cast<uint32_t>(m_entity_handle);
+        }
+
+        operator entt::entity() const
+        {
+            return m_entity_handle;
+        }
+
+        operator bool() const
+        {
+            if (auto registry = s_weak_registry_ptr.lock())
+            {
+                return (m_entity_handle != entt::null);
+            }
+            return false;
+        }
+
+    private:
+        entt::entity                         m_entity_handle{entt::null};
+        static std::weak_ptr<entt::registry> s_weak_registry_ptr;
+    };
+
+} // namespace ZEngine::Rendering::Entities
