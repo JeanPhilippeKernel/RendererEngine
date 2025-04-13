774dda0a9302218127002d0222e6e630f9d302f4
jnyfah <jnyfaah@gmail.com>
2025-04-13 18:14:31 +0100

    fomart

--- ZEngine/ZEngine/Core/Maths/Vec3.h
+++ ZEngine/ZEngine/Core/Maths/Vec3.h
@@ -11,10 +11,7 @@ namespace ZEngine::Core::Maths
         float y = 0.0f;
         float z = 0.0f;
 
-        Vec3() = default;
-        Vec3(float x, float y, float z): x(x), y(y), z(z) {}
-
-        
-
+        Vec3()  = default;
+        Vec3(float x, float y, float z) : x(x), y(y), z(z) {}
     };
-}
+} // namespace ZEngine::Core::Maths
