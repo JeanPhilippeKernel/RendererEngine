f054bade75d9a74b897d05386df14bc063482edc
Jean Philippe <jeanphilippe02666592@outlook.fr>
2025-05-25 02:57:56 +0100

    fixed matrix implementation

--- ZEngine/tests/Maths/Matrix_test.cpp
+++ ZEngine/tests/Maths/Matrix_test.cpp
@@ -9,8 +9,8 @@ TEST(MatrixTest, ConstructionAndIndexingFloat)
 {
     Mat2<float> m2(1.1f, 2.2f, 3.3f, 4.4f);
     EXPECT_NEAR(m2(0, 0), 1.1f, EPSILON);
-    EXPECT_NEAR(m2(1, 0), 2.2f, EPSILON);
-    EXPECT_NEAR(m2(0, 1), 3.3f, EPSILON);
+    EXPECT_NEAR(m2(1, 0), 3.3f, EPSILON);
+    EXPECT_NEAR(m2(0, 1), 2.2f, EPSILON);
     EXPECT_NEAR(m2(1, 1), 4.4f, EPSILON);
 
     Mat3<float> m3(1.0f, 2.0f, 3.0f, 4.0f, 5.0f, 6.0f, 7.0f, 8.0f, 9.0f);
@@ -31,5 +31,6 @@ TEST(MatrixTest, DeterminantFloat)
     EXPECT_NEAR(Determinant(m2), -2.0f, EPSILON);
 
     Mat3<float> m3(1, 2, 3, 0, 1, 4, 5, 6, 0);
+    auto        d = Determinant(m3);
     EXPECT_NEAR(Determinant(m3), 1.0f, EPSILON);
 }
